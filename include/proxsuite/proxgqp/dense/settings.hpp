26e1321f4df1e883be5b9d9094f65690718e17e2
Gauss_Taylor <gausstayloreuler@gmail.com>
2026-06-24 11:52:50 +0200

    Set back to 20 the max number of line search in armijo

--- include/proxsuite/proxgqp/dense/settings.hpp
+++ include/proxsuite/proxgqp/dense/settings.hpp
@@ -27,7 +27,7 @@ template <typename T> struct GQPSettings : proxsuite::proxqp::Settings<T> {
   GQPSettings()
       : proxsuite::proxqp::Settings<T>(), penaltyReduction(0.1),
         epsNewtonInit(1e-3), epsOuterInit(1e-2), lineSearchReduction(0.5),
-        armijoConstant(1e-4), maxLineSearchIters(5), min_search_step(1e-5),
+        armijoConstant(1e-4), maxLineSearchIters(20), min_search_step(1e-5),
         stepInCaseBelowMin(1), rhoIncreaseFactor(10), maxRho(1.0) {}
 };
 
